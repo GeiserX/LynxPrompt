89a67b5409260a2167a2d15cd88c2c727e24a53a
GeiserX <9169332+GeiserX@users.noreply.github.com>
2025-12-29 20:53:26 +0100

    chore(cli): bump version to 0.4.4

--- cli/chocolatey/tools/chocolateyinstall.ps1
+++ cli/chocolatey/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'lynxprompt'
-$version = '0.4.3'
+$version = '0.4.4'
 
 # Check if Node.js is installed
 $node = Get-Command node -ErrorAction SilentlyContinue
